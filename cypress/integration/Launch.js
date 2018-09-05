f052df3821eaf521893d11ebd09b58a4eff965bd
BasanthKS <basanth.swain@gmail.com>
2018-09-05 16:21:27 +0100

    changed the description of the test

--- cypress/integration/Launch.js
+++ cypress/integration/Launch.js
@@ -1,5 +1,5 @@
 
-describe('The test home page',() => {
+describe('Assertion for a tab called new in in the home page',() => {
     it('successfully loads', ()=> {
 cy.visit('/')
 cy.get(".col1 > h2").contains("treat your inbox");
